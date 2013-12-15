1e48189b5697e262e3ea34f66d62cfb0cec937cc
rog1121 <rog1121@gmail.com>
2013-12-15 00:53:22 -0700

    fix

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,9 @@
          </div>
       </div>
       <footer>
-        <div class="bottom-ad"><p><img src="http://i1210.photobucket.com/albums/cc408/ninjapotamuspics/banner.png" width="100%" height="100%"></p></div>
+        <div class="bottom-ad">
+          <a href="http://doge.netcodepool.org/"><img src="http://i.imgur.com/47zcSAa.png" width="100%" height="100%"></p>
+        </div>
         <p style="font-size: 11px;">rog1121 & g2x3k &copy; 2011-2012 source: <a href='https://github.com/rog1121/litecoin-instawallet-simple'>Github</a> - exec: <?=round(timer()-$start,5)?> sec - Donate to: <?=$btclient->getaccountaddress($don_account);?> (recv: <?=$btclient->getbalance($don_account,0)?> DOGE)</p>
       </footer>
 
